e3ecaa2dd27cdbbb28582d8745182e1ad9d9cba3
pro-stak666 <mineev_kirill2005@mail.ru>
2023-11-29 18:17:26 +0300

    123

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
-def knapsack(C, weight, cost, n):
-    K = [[0 for x in range(C + 1)] for x in range(n + 1)]
-    for i in range(n + 1):
-        for j in range(C + 1):
-            if i == 0 or j == 0:
-                K[i][j] = 0
-            elif weight[i - 1] <= j:
-                K[i][j] = max(cost[i - 1] + K[i - 1][j - weight[i - 1]], K[i - 1][j])
-            else:
-                K[i][j] = K[i - 1][j]
-    return K
+# Вариант 10, Начальное кол-во очков выживаемости равно 0,
+# так как я вычел сразу антидот и ингалятор за недаобностью
+
+def merge(seq):
+    merged = []
+    for s in seq:
+        for x in s:
+            merged.append(x)
+    return merged
 
 
-items = ['rifle', 'pistol', 'ammo', 'medkit', 'inhaler', 'knife',
-        'axe', 'talisman', 'flask', 'food', 'crossbow']
-values = [25, 15, 15, 20, 5, 15, 20, 25, 15, 20, 20]
-weights = [3, 2, 2, 2, 1, 1, 3, 1, 1, 2, 2]
-capacity = 8
-all_points = sum(values)
-init_points = 25
-count = len(values)
+things = [
+    ('Винтовка', 'в', 3, 25),
+    ('Пистолет', 'п', 2, 15),
+    ('Боекомплект', 'б', 2, 15),
+    ('Аптечка', 'а', 2, 20),
+    ('Нож', 'н', 1, 15),
+    ('Топор', 'т', 3, 20),
+    ('Оберег', 'о', 1, 25),
+    ('Фляжка', 'ф', 1, 15),
+    ('Еда', 'к', 2, 20),
+    ('Арбалет', 'р', 2, 20)
+]
 
-K = knapsack(capacity, weights, values, count)
+bag = [['' for _ in range(3)] for _ in range(3)]
+current_point = 0
+cells = 9
+things.sort(key=lambda x: (x[-1], x[-2]), reverse=False)
+res = []
+for i in things:
+    if cells - i[2] >= 0:
+        current_point += i[3]
+        cells -= i[2]
+        res.append(i[1:3])
+    else:
+        current_point -= i[3]
 
-i, j, total = count, capacity, 0
-res = K[count][capacity]
-while i > 0 and res > 0:
-    if res != K[i - 1][j]:
-        print(items[i - 1], "value", values[i - 1], "points,", weights[i - 1], "cells")
-        total += weights[i - 1]
-        res -= values[i - 1]
-        j -= weights[i - 1]
-    i -= 1
-print("\nTotal weight:", total, "\b, knapsack value:", K[count][capacity])
-points = K[count][capacity] + init_points - (all_points - K[count][capacity])
-print("Overall value", all_points)
-print("Total value", points)
-
+res = merge([list(i[1] * i[0]) for i in res])
+for i in range(3):
+    for j in range(3):
+        bag[i][j] = res[i * 3 + j]
+for i in bag:
+    print(i)
